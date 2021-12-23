e52c3077f040a32f08b47d6c692891af605b8399
Gokay Okutucu <gokay.okutucu@felixo.com>
2021-12-23 23:56:36 +0300

    Add some view models

---
+++ src/Services/Fly.Application/DomainEvents/Products/ViewModels/GetProductViewModel.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Fly.Application.Common.Mappings;
+using Fly.Application.DomainEvents.Products.Dtos;
+
+namespace Fly.Application.DomainEvents.Products.ViewModels
+{
+    public class GetCategoryViewModel : IMapFrom<CategoryDto>
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CategoryDto, GetCategoryViewModel>().ReverseMap();
+        }
+    }
+
+    public class GetProductViewModel : IMapFrom<ProductDto>
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CategoryId { get; set; }
+        public GetCategoryViewModel Category { get; set; }
+        public decimal Price { get; set; }
+        public string Currency { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CategoryDto, GetCategoryViewModel>().ReverseMap();
+            profile.CreateMap<ProductDto, GetProductViewModel>().ReverseMap();
+        }
+
+    }
+}
---
+++ src/Services/Fly.Application/DomainEvents/Products/ViewModels/PostProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Fly.Application.Common.Mappings;
+using Fly.Application.DomainEvents.Products.Dtos;
+
+namespace Fly.Application.DomainEvents.Products.ViewModels
+{
+    public class PostProductViewModel : IMapFrom<ProductDto>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CategoryId { get; set; }
+        public decimal Price { get; set; }
+        public string Currency { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<ProductDto, PostProductViewModel>()
+                .ReverseMap();
+        }
+    }
+}
